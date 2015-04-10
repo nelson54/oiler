5348840599e1502463f8c8c4ff3de0229c3ceb93
Derek Nelson <dereknelson01@gmail.com>
2015-04-10 09:15:25 -0400

    clean up 5

--- euler5.py
+++ euler5.py
@@ -1,7 +1,6 @@
 from fractions import gcd
 from functools import reduce
 
-def lcm(numbers):
-    return reduce(lambda x, y: (x*y)/gcd(x, y), numbers, 1)
+lcm = lambda numbers: reduce(lambda x, y: (x*y)/gcd(x, y), numbers, 1)
 
 print(lcm(range(1, 20)))
