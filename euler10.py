e554fbe67de5588937c8a93e451c9682e0136b36
Derek <dereknelson01@gmail.com>
2015-04-12 19:53:02 -0400

    fix to problem 10

--- euler10.py
+++ euler10.py
@@ -1,4 +1,4 @@
 from itertools import count
 
 
-print(sum((x for x in range(2000000) if not any(i for i in range(2, x) if x % i is 0))))
+print(sum((x for x in range(2, 2000000) if not any(i for i in range(2, x) if x % i is 0))))
